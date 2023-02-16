5e4a10c25746988020eaa4ec2a70b507316b511f
Suleman1413 <msulaman061@gmail.com>
2023-02-16 08:32:37 +0500

    testing file

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-from test import Student
+if __name__ == '__main__':
+    x = int(input("Input of x"))
+    y = int(input("Input of y"))
+    z = int(input("Input of z"))
+    n = int(input("Input of n"))
 
+    print(list([i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1)  if i+j+k !=n))
