132f63b7e79cf22aabd2f081abf036a31c3d2a55
Suleman1413 <msulaman061@gmail.com>
2023-02-18 09:57:24 +0500

    testing file

---
+++ #4members_outsideClass.py
@@ -0,0 +1,20 @@
+print("Class members outside the class")
+
+class Cars:
+    def __init__(self, name, model):
+        self.carname = name
+        self.carmodel = model
+
+    def display(self):
+        print(f"The car name is {self.carname} and car model is {self.carmodel}")
+
+car1=Cars('honda', 'city')
+
+
+#accessing attributes outside the class
+car1.carname = 'Honda'
+print(car1.carname)
+print(car1.carmodel)
+
+#accessing action outside the class
+car1.display()
