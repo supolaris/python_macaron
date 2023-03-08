a64537a0567a1f9d2a7d3ef6a7eb0af7b134c717
Suleman1413 <msulaman061@gmail.com>
2023-03-08 11:29:58 +0500

    super function of

---
+++ #14super_function.py
@@ -0,0 +1,17 @@
+class Teacher:
+    def __init__(self, name, clas):
+        self.tname = name
+        self.tclass = clas
+        print("This is teacher here")
+
+    def newFun(self):
+        print("this is another constructor")
+
+class Student(Teacher):
+    def __init__(self, name, clas):
+        super().__init__(name, clas)
+        super().newFun()
+        print("this is student")
+
+s1 = Student('ali',  632)
+print(s1.__dict__)
