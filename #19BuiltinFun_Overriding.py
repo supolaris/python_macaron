10bf0089dac0e85865569fbd5bef4f9fc24b1cbd
Suleman1413 <msulaman061@gmail.com>
2023-03-14 04:02:46 +0500

    over riding built in functions

---
+++ #19BuiltinFun_Overriding.py
@@ -0,0 +1,14 @@
+class myClass:
+    def __init__(self, name, dept, loc):
+        self.cname = name
+        self.cdept = dept
+        self.cloc = loc
+    def display(self):
+        print(self.cname, self.cloc, self.cdept)
+
+    def __len__(self):
+       return len(self.cname)+len(self.cloc)+len(self.cdept)
+
+myObj = myClass(["II, CS"], ["UIIT", "UMS"], ["Stadium", "Road"])
+print(myObj.display())
+print(myObj.__len__())
