868910a17dd62810e2ab90aa3e6ce00bb360603c
Suleman1413 <msulaman061@gmail.com>
2023-03-14 17:48:00 +0500

    bfs code

---
+++ #20bfs.py
@@ -0,0 +1,27 @@
+graph = {
+    '5': ['3', '7'],
+    '3': ['2', '4'],
+    '7': ['8'],
+    '2': [],
+    '4': ['8'],
+    '8': []
+}
+
+visited = []
+queued = []
+
+def bfs(visited, graph, node):
+    visited.append(node)
+    queued.append(node)
+
+    while queued:
+        m = queued.pop(0)
+        print(m, end = ' ')
+
+        for neighbour in graph[m]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queued.append(neighbour)
+
+print("Print the bfs")
+bfs(visited, graph, '5')
